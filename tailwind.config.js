84f6cfd2ddfab8380da0545be574cef9f5c0317b
Groovesgt <brayanrodrive@gmail.com>
2023-02-28 16:59:23 -0500

    Responsive Nav

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        'pixel': ['Press Start 2P', 'cursive'],
+      }
+    },
+  },
+  
+  plugins: [],
+}
